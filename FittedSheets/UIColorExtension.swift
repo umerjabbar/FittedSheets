75fb5b6f9b7a50c92dbd7054430bb0685f3d9deb
Umer <umer@umax.local>
2026-07-03 01:36:56 +0500

    Fix swapped light/dark colors in UIColor appearance initializer
    UIColor(light:dark:) returned the light color for the .dark interface style and
    the dark color otherwise, inverting appearance. Return the matching color for
    each style, and drop the now-unnecessary iOS 13 availability fallbacks.

--- FittedSheets/UIColorExtension.swift
+++ FittedSheets/UIColorExtension.swift
@@ -6,54 +6,42 @@
 //  Copyright © 2020 Gordon Tucker. All rights reserved.
 //
 
-#if os(iOS) || os(tvOS) || os(watchOS)
+#if os(iOS)
 import UIKit
 
 extension UIColor {
     convenience init(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat = 1, darkRed: CGFloat, darkGreen: CGFloat, darkBlue: CGFloat, darkAlpha: CGFloat = 1) {
-        if #available(iOS 13.0, *) {
-            self.init { (traits) -> UIColor in
-                switch traits.userInterfaceStyle {
-                    case .dark:
-                        return UIColor(red: darkRed, green: darkGreen, blue: darkBlue, alpha: darkAlpha)
-                    default:
-                        return UIColor(red: red, green: green, blue: blue, alpha: alpha)
-                }
+        self.init { traits in
+            switch traits.userInterfaceStyle {
+                case .dark:
+                    return UIColor(red: darkRed, green: darkGreen, blue: darkBlue, alpha: darkAlpha)
+                default:
+                    return UIColor(red: red, green: green, blue: blue, alpha: alpha)
             }
-        } else {
-            self.init(red: red, green: green, blue: blue, alpha: alpha)
         }
     }
-    
+
     convenience init(white: CGFloat, alpha: CGFloat = 1, black: CGFloat, darkAlpha: CGFloat = 1) {
-        if #available(iOS 13.0, *) {
-            self.init { (traits) -> UIColor in
-                switch traits.userInterfaceStyle {
-                    case .dark:
-                        return UIColor(white: black, alpha: darkAlpha)
-                    default:
-                        return UIColor(white: white, alpha: alpha)
-                }
+        self.init { traits in
+            switch traits.userInterfaceStyle {
+                case .dark:
+                    return UIColor(white: black, alpha: darkAlpha)
+                default:
+                    return UIColor(white: white, alpha: alpha)
             }
-        } else {
-            self.init(white: white, alpha: alpha)
         }
     }
-    
+
     convenience init(light: UIColor, dark: UIColor) {
-        if #available(iOS 13.0, *) {
-            self.init { (traits) -> UIColor in
-                switch traits.userInterfaceStyle {
-                    case .dark:
-                        return light
-                    default:
-                        return dark
-                }
+        self.init { traits in
+            switch traits.userInterfaceStyle {
+                case .dark:
+                    return dark
+                default:
+                    return light
             }
-        } else {
-            self.init(cgColor: light.cgColor)
         }
     }
 }
 
-#endif // os(iOS) || os(tvOS) || os(watchOS)
+#endif // os(iOS)
